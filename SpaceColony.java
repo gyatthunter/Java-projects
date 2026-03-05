3566347d944561c59835883bd33adf069027fc16
gyatthunter <soumodityapal2004@gmail.com>
2026-03-05 16:19:21 -0500

    step 1, complete

--- SpaceColony.java
+++ SpaceColony.java
@@ -1,15 +1,19 @@
 public class SpaceColony {
-            private int oxygen = 100;
-            private int food = 100;
-            private int energy = 100;
-            private int population = 10;
-            
-            public void printer () {
+    public static void main (String [] args){
+                SpaceColony colony = new SpaceColony();
+                colony.printer();
+                }
+
+                private int oxygen = 100;
+                private int food = 100;
+                private int energy = 100;
+                private int population = 10;
+
+                public void printer () {
                 System.out.println ("Colony-Status!");
                 System.out.println ("Oxygen = " +oxygen);
                 System.out.println ("Food = " +food);
                 System.out.println ("Population = " +energy);
                 System.out.println ("Population = " +population);
-            }
-
+                }
 }
